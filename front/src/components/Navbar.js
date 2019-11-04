e75a8b2558249a851012fb859ed3a89487699e6c
Camilo Beltran <ca.beltran10@uniandes.edu.co>
2019-11-04 10:20:55 -0500

    Fetch

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -15,69 +15,77 @@ class Navbar extends Component {
     };
   }
 
-  googleSDK() {
-    window["googleSDKLoaded"] = () => {
-      window["gapi"].load("auth2", () => {
-        this.auth2 = window["gapi"].auth2.init({
-          client_id:
-            "917120081957-a0798ml3atas8sr08h70s75aqifb6jqj.apps.googleusercontent.com",
-          cookiepolicy: "single_host_origin",
-          scope: "profile email"
-        });
-        this.prepareLoginButton();
-      });
-    };
-
-    (function(d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {
-        return;
-      }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://apis.google.com/js/platform.js?onload=googleSDKLoaded";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "google-jssdk");
-  }
-
-  componentDidMount() {
-    this.googleSDK();
-  }
-
-  prepareLoginButton = () => {
-    this.auth2.attachClickHandler(
-      this.refs.googleLoginBtn,
-      {},
-      googleUser => {
-        let profile = googleUser.getBasicProfile();
-        this.setState({
-          isSignedIn: true,
-          token: googleUser.getAuthResponse().id_token,
-          id: profile.getId(),
-          name: profile.getName(),
-          url: profile.getImageUrl(),
-          email: profile.getEmail()
-        });
-        console.log(this.state.isSignedIn);
-      },
-      error => {
-        alert(JSON.stringify(error, undefined, 2));
-      }
-    );
-  };
-
-  handleClick = e => {
-    e.preventDefault();
-    this.setState({
-      isSignedIn: false,
-      token: "",
-      id: "",
-      name: "",
-      url: "",
-      email: ""
+  fetchLogin() {
+    fetch("/auth/google", {
+      crossDomain: true,
+      method: "GET",
+      headers: { "Content-Type": "application/json" }
     });
-  };
+  }
+
+  // googleSDK() {
+  //   window["googleSDKLoaded"] = () => {
+  //     window["gapi"].load("auth2", () => {
+  //       this.auth2 = window["gapi"].auth2.init({
+  //         client_id:
+  //           "917120081957-a0798ml3atas8sr08h70s75aqifb6jqj.apps.googleusercontent.com",
+  //         cookiepolicy: "single_host_origin",
+  //         scope: "profile email"
+  //       });
+  //       this.prepareLoginButton();
+  //     });
+  //   };
+
+  //   (function(d, s, id) {
+  //     var js,
+  //       fjs = d.getElementsByTagName(s)[0];
+  //     if (d.getElementById(id)) {
+  //       return;
+  //     }
+  //     js = d.createElement(s);
+  //     js.id = id;
+  //     js.src = "https://apis.google.com/js/platform.js?onload=googleSDKLoaded";
+  //     fjs.parentNode.insertBefore(js, fjs);
+  //   })(document, "script", "google-jssdk");
+  // }
+
+  // componentDidMount() {
+  //   this.googleSDK();
+  // }
+
+  // prepareLoginButton = () => {
+  //   this.auth2.attachClickHandler(
+  //     this.refs.googleLoginBtn,
+  //     {},
+  //     googleUser => {
+  //       let profile = googleUser.getBasicProfile();
+  //       this.setState({
+  //         isSignedIn: true,
+  //         token: googleUser.getAuthResponse().id_token,
+  //         id: profile.getId(),
+  //         name: profile.getName(),
+  //         url: profile.getImageUrl(),
+  //         email: profile.getEmail()
+  //       });
+  //       console.log(this.state.isSignedIn);
+  //     },
+  //     error => {
+  //       alert(JSON.stringify(error, undefined, 2));
+  //     }
+  //   );
+  // };
+
+  // handleClick = e => {
+  //   e.preventDefault();
+  //   this.setState({
+  //     isSignedIn: false,
+  //     token: "",
+  //     id: "",
+  //     name: "",
+  //     url: "",
+  //     email: ""
+  //   });
+  // };
 
   render() {
     return (
@@ -97,7 +105,7 @@ class Navbar extends Component {
               OUR HISTORY
             </Link>
             {!this.state.isSignedIn ? (
-              <button className="sessionBotton" ref="googleLoginBtn">
+              <button className="sessionBotton" onClick={this.fetchLogin}>
                 LOGIN
               </button>
             ) : (
