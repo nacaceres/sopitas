281aa89ef9f9371cc08b64eba4a29fff1cb3a921
Camilo Beltran <ca.beltran10@uniandes.edu.co>
2019-11-04 21:06:52 -0500

    README

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
 
 import Header from "./components/Header";
