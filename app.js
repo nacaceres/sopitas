3c317e73ad9432b4816574de74d5688b8b830e33
carlosmendoza <ca.mendoza@uniandes.edu.co>
2019-12-08 22:53:28 -0500

    API de pagos

--- app.js
+++ app.js
@@ -50,9 +50,120 @@ app.use("/", indexRouter);
 app.use("/auth", authGoogleRouter);
 app.use("/authLocal", authLocalRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+
+
+
+
+
+
+
+//----------------------
+
+const { resolve } = require("path");
+// Copy the .env.example in the root into a .env file in this folder
+const env = require("dotenv").config({ path: "./.env" });
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+app.use(express.static(process.env.STATIC_DIR));
+app.use(
+  express.json({
+    // We need the raw body to verify webhook signatures.
+    // Let's compute it only when hitting the Stripe webhook endpoint.
+    verify: function(req, res, buf) {
+      if (req.originalUrl.startsWith("/webhook")) {
+        req.rawBody = buf.toString();
+      }
+    }
+  })
+);
+
+
+
+app.get("/config", (req, res) => {
+  res.send({
+    publicKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    basePrice: process.env.BASE_PRICE,
+    currency: process.env.CURRENCY
+  });
+});
+
+// Fetch the Checkout Session to display the JSON result on the success page
+app.get("/checkout-session", async (req, res) => {
+  const { sessionId } = req.query;
+  const session = await stripe.checkout.sessions.retrieve(sessionId);
+  res.send(session);
+});
+
+app.post("/create-checkout-session", async (req, res) => {
+  const domainURL = process.env.DOMAIN;
+
+  const { quantity } = req.body;
+  const {name} = req.body;
+  const {cost} = req.body;
+  const {image}= req.body;
+  // Create new Checkout Session for the order
+  // Other optional params include:
+  // [billing_address_collection] - to display billing address details on the page
+  // [customer] - if you have an existing Stripe Customer ID
+  // [payment_intent_data] - lets capture the payment later
+  // [customer_email] - lets you prefill the email input in the form
+  // For full details see https://stripe.com/docs/api/checkout/sessions/create
+  session = await stripe.checkout.sessions.create({
+    payment_method_types: process.env.PAYMENT_METHODS.split(", "),
+    line_items: [
+      {
+        name: "Plan",
+        images: [image],
+        quantity: quantity,
+        currency: process.env.CURRENCY,
+        amount: cost // Keep the amount on the server to prevent customers from manipulating on client
+      }
+    ],
+    // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+    success_url: `http://localhost:3000`,
+    cancel_url: `http://localhost:3001`
+  });
+
+  res.send({
+    sessionId: session.id
+  });
+});
+
+// Webhook handler for asynchronous events.
+app.post("/webhook", async (req, res) => {
+  let data;
+  let eventType;
+  // Check if webhook signing is configured.
+  if (process.env.STRIPE_WEBHOOK_SECRET) {
+    // Retrieve the event by verifying the signature using the raw body and secret.
+    let event;
+    let signature = req.headers["stripe-signature"];
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.rawBody,
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`);
+      return res.sendStatus(400);
+    }
+    // Extract the object from the event.
+    data = event.data;
+    eventType = event.type;
+  } else {
+    // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+    // retrieve the event data directly from the request body.
+    data = req.body.data;
+    eventType = req.body.type;
+  }
+
+  if (eventType === "checkout.session.completed") {
+    console.log(`🔔  Payment received!`);
+  }
+
+  res.sendStatus(200);
 });
 
 // error handler
@@ -65,5 +176,13 @@ app.use(function(err, req, res) {
   res.status(err.status || 500);
   res.render("error");
 });
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
 
 module.exports = app;
